f7668976d81942681f9088e4893289ffa905e195
MyckollWinchester <myckoll.winchester@usm.cl>
2025-05-01 22:37:33 -0400

    chore: fix Navbar.tsx formatting for standarization

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,16 +1,18 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <nav className="bg-blue-600 p-4 text-white flex justify-between">
-      <Link to="/" className="font-bold text-xl">Conectados</Link>
+      <Link to="/" className="font-bold text-xl">
+        Conectados
+      </Link>
       <div className="space-x-4">
         <Link to="/">Inicio</Link>
         <Link to="/registro">Registro</Link>
         <Link to="/login">Iniciar Sesión</Link>
       </div>
     </nav>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
