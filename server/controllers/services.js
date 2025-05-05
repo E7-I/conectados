bad7be60697922240a153e38b413407b94695293
Rodrigo-Alfaroo <rodrigo.alfaroo@usm.cl>
2025-05-04 20:39:23 -0400

    DP-13 page servicios + backend working

--- server/controllers/services.js
+++ server/controllers/services.js
@@ -104,11 +104,38 @@ const addRating = async (req, res) => {
     }
 };
 
+const getFilteredServices = async (req, res) => {
+    try {
+      const { minPrice, maxPrice, category, minRating } = req.query;
+  
+      const filters = {};
+  
+      if (minPrice || maxPrice) {
+        filters['price.min'] = { ...(minPrice && { $gte: Number(minPrice) }) };
+        filters['price.max'] = { ...(maxPrice && { $lte: Number(maxPrice) }) };
+      }
+  
+      if (category) {
+        filters.categories = category;
+      }
+  
+      if (minRating) {
+        filters.averageRating = { $gte: Number(minRating) };
+      }
+  
+      const services = await Service.find(filters);
+      res.json(services);
+    } catch (error) {
+      res.status(500).json({ error: 'Error fetching services' });
+    }
+};
+
 export default {
     newService,
     getServiceId,
     getAllServices,
     getServiceProfessionalId,
     updateService,
-    addRating
+    addRating,
+    getFilteredServices
 };
