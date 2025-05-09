bdbdd5135f1b9378298afcf2dd9673e5d4f62af0
MyckollWinchester <myckoll.winchester@usm.cl>
2025-05-09 19:46:08 -0400

    add authentication middleware to user routes

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import userController from '../controllers/user.js'
+import { auth } from '../middlewares/auth.js'
 
 const router = express.Router()
 
@@ -7,7 +8,7 @@ router.post('/register', userController.register)
 router.post('/login', userController.login)
 router.get('/id/:id', userController.getUserById)
 router.get('/search', userController.searchUsers)
-router.get('/', userController.getAllUsers)
+router.get('/', auth, userController.getAllUsers)
 router.put('/update/:id', userController.updateUser)
 
 export default router
